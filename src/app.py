cde65164fdfa2502915e98c3a1a08f038b361b25
Dalyn Lara <77414220+DalynLC@users.noreply.github.com>
2025-03-12 08:21:18 +0000

    graficas 1/2

--- src/app.py
+++ src/app.py
@@ -5,9 +5,7 @@ import numpy as np
 from pickle import load
 
 #carga el modelo
-model = load(open("../src/model_classifier.sav", "rb"))
-
-st.set_page_config(page_title="Predictor de Accidentes de Tráfico", page_icon=":oncoming_automobile:", layout="wide")
+#model = load(open("../src/model_classifier.sav", "rb"))
 
 page_element="""
 <style>
@@ -22,19 +20,38 @@ page_element="""
 }
 </style>
 """
-
+st.set_page_config(page_title="Predictor de Accidentes de Tráfico", page_icon=":oncoming_automobile:", layout="wide")
 st.markdown(page_element, unsafe_allow_html=True)
 
 with st.container():
-    st.title(":blue_car: Predictor de Lesiones en Accidentes de Tráfico :car:")
-    st.markdown('<div class="my-container">', unsafe_allow_html=True)
+    st.title(":blue_car: Predictor de Lesiones en Accidentes de Tráfico :car:",)
     
     st.write("Esta aplicación permite predecir la gravedad de las heridas en un accidente de tráfico.")
-    st.markdown('</div>', unsafe_allow_html=True)
+
 
 with st.container():
-    st.write("Fecha: ")
-    st.write("Hora: ")
+    col1, col2 = st.columns(2)
+
+days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"] 
+month = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+
+fecha = dt.date.today()
+dDay = days[fecha.weekday()] 
+dMonth = month[fecha.month-1]
+dDayN = str(fecha.day)
+dYear = str(fecha.year)
+
+with st.container(border=True):
+    col1, col2, col3 = st.columns(3)
+    with col1: 
+        #st.write("Date: ", dDay, " ", dDayN, " ", dMonth," ", dYear)
+        crash_day = st.selectbox('Day',["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"])
+    with col2:    
+        crash_month = st.selectbox('Month',["January","February","March","April","May","June","July","August","September","October","November","December"])
+    with col3:
+        crash_hour = st.selectbox('Time',[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24])
+
+with st.container(border=True):
     st.subheader("🔍 Ingresa las condiciones del accidente")
     col1, col2 = st.columns(2)
     with col1:
@@ -52,11 +69,11 @@ with st.container():
         total_fatal_injuries = st.slider('Fatal Injuries',0,10)
         total_non_fatal_injuries= st.slider('Non Fatal Injuries',0,21)
 
-with st.container():
+with st.container(border=True):
     st.markdown("---")
     if st.button("✨ Predict Most Severe Injury"):
         input_data = np.array([[traffic_control_device,weather_condition,alignment,roadway_surface_cond,crash_type,intersection_related_i,prim_contributory_cause,num_units,total_fatal_injuries,total_non_fatal_injuries]])
-        prediction = model.predict(input_data)[0]
+        #prediction = model.predict(input_data)[0]
 
         #st.success(f"🏡 El precio predicho de la casa es: **${prediction * 1000:.2f} USD**")
 
